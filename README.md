ef384a589e833d77b209967e4442433948eb906d
Dusan Klinec <dusan.klinec@gmail.com>
2019-01-03 14:01:34 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vue select
+
+Simple Vue.js select component with potential autocomplete (remote loading), tags, validation, filtering and editing.
+
+Similar to https://github.com/sagalbot
+
+
---
+++ vue-select/src/mixins/index.js
@@ -0,0 +1,6 @@
+import ajax from './ajax'
+import pointer from './typeAheadPointer'
+import pointerScroll from './pointerScroll'
+import keyMaps from './keyMaps'
+
+export default { ajax, pointer, pointerScroll, keyMaps }
